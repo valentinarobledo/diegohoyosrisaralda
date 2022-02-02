d3e0734ea4fca6f2048e8a06c7923c9eb59f501a
valentinarobledo <valenrobledo31@gmail.com>
2022-02-01 23:57:49 -0500

    Pactos

---
+++ js/swiper.js
@@ -0,0 +1,8 @@
+var swiper = new Swiper(".mySwiper", {
+  autoHeight: true,
+  mousewheel: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
